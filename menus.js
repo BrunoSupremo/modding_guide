e1df1b1ad8fa519fa89cae2f6e5b58baa3c63bf1
Bruno Mussoi Mendonça <bruno_mussoi@hotmail.com>
2023-01-27 23:36:29 -0300

    Ops, wrong relative paths :(
    Github pages have a weird way to work with paths
    The root starts at the project, one folder higher than the site root
    It ruins html relative paths
    You need to prefix them all with the site folder

--- menus.js
+++ menus.js
@@ -1,136 +1,136 @@
 document.currentScript.outerHTML=`
 <header>
-	<img src="/images/stonehearth-logo.png"/>
+	<img src="/modding_guide/images/stonehearth-logo.png"/>
 </header>
 <a id="go_to_the_top_button" href="#top">TOP</a>
 <nav>
-	<a href="/index.html">Stonehearth's Modding Guide</a>
+	<a href="/modding_guide/index.html">Stonehearth's Modding Guide</a>
 	<section>
-		<a href="/modding_guide/essentials/index.html">Modding Essentials</a>
+		<a href="/modding_guide/modding_guide/essentials/index.html">Modding Essentials</a>
 		<div>
-			<a href="/modding_guide/essentials/managing/index.html">Managing mods</a>
+			<a href="/modding_guide/modding_guide/essentials/managing/index.html">Managing mods</a>
 			<div>
-				<a href="/modding_guide/essentials/managing/workshop/index.html">Steam Workshop mods</a>
-				<a href="/modding_guide/essentials/managing/unmanaged/index.html">Local mods</a>
+				<a href="/modding_guide/modding_guide/essentials/managing/workshop/index.html">Steam Workshop mods</a>
+				<a href="/modding_guide/modding_guide/essentials/managing/unmanaged/index.html">Local mods</a>
 			</div>
-			<a href="/modding_guide/essentials/programming_languages/index.html">Programming Languages</a>
-			<a href="/modding_guide/essentials/modding_scope/index.html">What can be modded</a>
-			<a href="/modding_guide/essentials/tools/index.html">Tools for modding</a>
-			<a href="/modding_guide/essentials/stonehearth/index.html">The Stonehearth mod</a>
-			<a href="/modding_guide/essentials/json/index.html">Small intro to JSON</a>
-			<a href="/modding_guide/essentials/manifest/index.html">The manifest</a>
-			<a href="/modding_guide/essentials/entities_components/index.html">Entities and components</a>
-			<a href="/modding_guide/essentials/mixins/index.html">Mixins</a>
-			<a href="/modding_guide/essentials/mixintos_overrides/index.html">Mixintos and overrides</a>
-			<a href="/modding_guide/essentials/removing/index.html">Mixintypes</a>
-			<a href="/modding_guide/essentials/localization/index.html">Localization</a>
-			<a href="/modding_guide/essentials/debugging/index.html">How to debug your errors</a>
-			<a href="/modding_guide/essentials/debugtools/index.html">Using debug tools</a>
+			<a href="/modding_guide/modding_guide/essentials/programming_languages/index.html">Programming Languages</a>
+			<a href="/modding_guide/modding_guide/essentials/modding_scope/index.html">What can be modded</a>
+			<a href="/modding_guide/modding_guide/essentials/tools/index.html">Tools for modding</a>
+			<a href="/modding_guide/modding_guide/essentials/stonehearth/index.html">The Stonehearth mod</a>
+			<a href="/modding_guide/modding_guide/essentials/json/index.html">Small intro to JSON</a>
+			<a href="/modding_guide/modding_guide/essentials/manifest/index.html">The manifest</a>
+			<a href="/modding_guide/modding_guide/essentials/entities_components/index.html">Entities and components</a>
+			<a href="/modding_guide/modding_guide/essentials/mixins/index.html">Mixins</a>
+			<a href="/modding_guide/modding_guide/essentials/mixintos_overrides/index.html">Mixintos and overrides</a>
+			<a href="/modding_guide/modding_guide/essentials/removing/index.html">Mixintypes</a>
+			<a href="/modding_guide/modding_guide/essentials/localization/index.html">Localization</a>
+			<a href="/modding_guide/modding_guide/essentials/debugging/index.html">How to debug your errors</a>
+			<a href="/modding_guide/modding_guide/essentials/debugtools/index.html">Using debug tools</a>
 			<div>
-				<a href="/modding_guide/essentials/debugtools/debugtools_mod/index.html">The debugtools mod</a>
-				<a href="/modding_guide/essentials/debugtools/default_console/index.html">The default console</a>
+				<a href="/modding_guide/modding_guide/essentials/debugtools/debugtools_mod/index.html">The debugtools mod</a>
+				<a href="/modding_guide/modding_guide/essentials/debugtools/default_console/index.html">The default console</a>
 			</div>
-			<a href="/modding_guide/essentials/startermods/index.html">Startermod example</a>
-			<a href="/modding_guide/essentials/shed/index.html">How to save time with SHED</a>
+			<a href="/modding_guide/modding_guide/essentials/startermods/index.html">Startermod example</a>
+			<a href="/modding_guide/modding_guide/essentials/shed/index.html">How to save time with SHED</a>
 		</div>
-		<a href="/modding_guide/basic/index.html">Basic Modding</a>
+		<a href="/modding_guide/modding_guide/basic/index.html">Basic Modding</a>
 		<div>
-			<a href="/modding_guide/basic/art/index.html">Art Workflow</a>
+			<a href="/modding_guide/modding_guide/basic/art/index.html">Art Workflow</a>
 			<div>
-				<a href="/modding_guide/basic/art/modeling/index.html">Creating models</a>
-				<a href="/modding_guide/basic/art/icons/index.html">Creating 2D assets</a>
-				<a href="/modding_guide/basic/art/vfx/index.html">Creating effects</a>
+				<a href="/modding_guide/modding_guide/basic/art/modeling/index.html">Creating models</a>
+				<a href="/modding_guide/modding_guide/basic/art/icons/index.html">Creating 2D assets</a>
+				<a href="/modding_guide/modding_guide/basic/art/vfx/index.html">Creating effects</a>
 				<div>
-					<a href="/modding_guide/basic/art/vfx/animations/index.html">Animations</a>
+					<a href="/modding_guide/modding_guide/basic/art/vfx/animations/index.html">Animations</a>
 					<div>
-						<a href="/modding_guide/basic/art/vfx/animations/anim_existing/index.html">For existing entities</a>
-						<a href="/modding_guide/basic/art/vfx/animations/anim_custom/index.html">For custom entities</a>
+						<a href="/modding_guide/modding_guide/basic/art/vfx/animations/anim_existing/index.html">For existing entities</a>
+						<a href="/modding_guide/modding_guide/basic/art/vfx/animations/anim_custom/index.html">For custom entities</a>
 					</div>
-					<a href="/modding_guide/basic/art/vfx/cubemitters/index.html">Particle effects</a>
-					<a href="/modding_guide/basic/art/vfx/animated_lights/index.html">Light effects</a>
-					<a href="/modding_guide/basic/art/vfx/other_effects/index.html">Other effects</a>
+					<a href="/modding_guide/modding_guide/basic/art/vfx/cubemitters/index.html">Particle effects</a>
+					<a href="/modding_guide/modding_guide/basic/art/vfx/animated_lights/index.html">Light effects</a>
+					<a href="/modding_guide/modding_guide/basic/art/vfx/other_effects/index.html">Other effects</a>
 				</div>
 			</div>
-			<a href="/modding_guide/basic/adding_items/index.html">Adding new items</a>
+			<a href="/modding_guide/modding_guide/basic/adding_items/index.html">Adding new items</a>
 			<div>
-				<a href="/modding_guide/basic/adding_items/entity_forms/index.html">Entity Forms</a>
-				<a href="/modding_guide/basic/adding_items/cloning_manually/index.html">Adding items manually</a>
-				<a href="/modding_guide/basic/adding_items/cloning_with_shed/index.html">Adding items with SHED</a>
-				<a href="/modding_guide/basic/adding_items/item_scale/index.html">Changing the scale</a>
+				<a href="/modding_guide/modding_guide/basic/adding_items/entity_forms/index.html">Entity Forms</a>
+				<a href="/modding_guide/modding_guide/basic/adding_items/cloning_manually/index.html">Adding items manually</a>
+				<a href="/modding_guide/modding_guide/basic/adding_items/cloning_with_shed/index.html">Adding items with SHED</a>
+				<a href="/modding_guide/modding_guide/basic/adding_items/item_scale/index.html">Changing the scale</a>
 			</div>
-			<a href="/modding_guide/basic/catalog/index.html">The catalog</a>
-			<a href="/modding_guide/basic/generic_resources/index.html">Generic resources</a>
-			<a href="/modding_guide/basic/craftables/index.html">Adding recipes</a>
-			<a href="/modding_guide/basic/wearables/index.html">Adding equipment</a>
+			<a href="/modding_guide/modding_guide/basic/catalog/index.html">The catalog</a>
+			<a href="/modding_guide/modding_guide/basic/generic_resources/index.html">Generic resources</a>
+			<a href="/modding_guide/modding_guide/basic/craftables/index.html">Adding recipes</a>
+			<a href="/modding_guide/modding_guide/basic/wearables/index.html">Adding equipment</a>
 			<div>
-				<a href="/modding_guide/basic/wearables/armor/index.html">Armor and hats</a>
-				<a href="/modding_guide/basic/wearables/weapons/index.html">Weapons</a>
+				<a href="/modding_guide/modding_guide/basic/wearables/armor/index.html">Armor and hats</a>
+				<a href="/modding_guide/modding_guide/basic/wearables/weapons/index.html">Weapons</a>
 			</div>
-			<a href="/modding_guide/basic/storages/index.html">Storage items</a>
-			<a href="/modding_guide/basic/color_maps/index.html">Color and material maps</a>
+			<a href="/modding_guide/modding_guide/basic/storages/index.html">Storage items</a>
+			<a href="/modding_guide/modding_guide/basic/color_maps/index.html">Color and material maps</a>
 			<div>
-				<a href="/modding_guide/basic/color_maps/shaders/index.html">Materials and shaders</a>
+				<a href="/modding_guide/modding_guide/basic/color_maps/shaders/index.html">Materials and shaders</a>
 			</div>
-			<a href="/modding_guide/basic/customization/index.html">Adding customizations</a>
-			<a href="/modding_guide/basic/crops/index.html">Adding crops</a>
-			<a href="/modding_guide/basic/renewable_resources/index.html">Renewable resources</a>
-			<a href="/modding_guide/basic/plants/index.html">Plants and trees</a>
-			<a href="/modding_guide/basic/static_scenarios/index.html">Static scenarios</a>
+			<a href="/modding_guide/modding_guide/basic/customization/index.html">Adding customizations</a>
+			<a href="/modding_guide/modding_guide/basic/crops/index.html">Adding crops</a>
+			<a href="/modding_guide/modding_guide/basic/renewable_resources/index.html">Renewable resources</a>
+			<a href="/modding_guide/modding_guide/basic/plants/index.html">Plants and trees</a>
+			<a href="/modding_guide/modding_guide/basic/static_scenarios/index.html">Static scenarios</a>
 			<div>
-				<a href="/modding_guide/basic/static_scenarios/ore_veins/index.html">Ore veins</a>
-				<a href="/modding_guide/basic/static_scenarios/landmarks/index.html">Landmarks</a>
-				<a href="/modding_guide/basic/static_scenarios/nests_ruins/index.html">Critter nests and ruins</a>
+				<a href="/modding_guide/modding_guide/basic/static_scenarios/ore_veins/index.html">Ore veins</a>
+				<a href="/modding_guide/modding_guide/basic/static_scenarios/landmarks/index.html">Landmarks</a>
+				<a href="/modding_guide/modding_guide/basic/static_scenarios/nests_ruins/index.html">Critter nests and ruins</a>
 			</div>
-			<a href="/modding_guide/basic/misc_json/index.html">Miscellaneous JSON</a>
+			<a href="/modding_guide/modding_guide/basic/misc_json/index.html">Miscellaneous JSON</a>
 			<div>
-				<a href="/modding_guide/basic/misc_json/shops/index.html">Loadouts and shops</a>
+				<a href="/modding_guide/modding_guide/basic/misc_json/shops/index.html">Loadouts and shops</a>
 			</div>
-			<a href="/modding_guide/basic/localization_mod/index.html">Translating mods</a>
+			<a href="/modding_guide/modding_guide/basic/localization_mod/index.html">Translating mods</a>
 		</div>
-		<a href="/modding_guide/intermediate/index.html">Intermediate Modding</a>
+		<a href="/modding_guide/modding_guide/intermediate/index.html">Intermediate Modding</a>
 		<div>
-			<a href="/modding_guide/intermediate/regions/index.html">Collision regions</a>
+			<a href="/modding_guide/modding_guide/intermediate/regions/index.html">Collision regions</a>
 			<div>
-				<a href="/modding_guide/intermediate/regions/origins/index.html">Origins</a>
-				<a href="/modding_guide/intermediate/regions/collision_destination/index.html">Collision and destination</a>
+				<a href="/modding_guide/modding_guide/intermediate/regions/origins/index.html">Origins</a>
+				<a href="/modding_guide/modding_guide/intermediate/regions/collision_destination/index.html">Collision and destination</a>
 			</div>
-			<a href="/modding_guide/intermediate/building_parts/index.html">Building parts</a>
-			<a href="/modding_guide/intermediate/commands/index.html">Adding commands</a>
-			<a href="/modding_guide/intermediate/buffs/index.html">Buffs</a>
-			<a href="/modding_guide/intermediate/thoughts/index.html">Thoughts</a>
-			<a href="/modding_guide/intermediate/traits/index.html">Traits</a>
-			<a href="/modding_guide/intermediate/biomes/index.html">Biomes</a>
+			<a href="/modding_guide/modding_guide/intermediate/building_parts/index.html">Building parts</a>
+			<a href="/modding_guide/modding_guide/intermediate/commands/index.html">Adding commands</a>
+			<a href="/modding_guide/modding_guide/intermediate/buffs/index.html">Buffs</a>
+			<a href="/modding_guide/modding_guide/intermediate/thoughts/index.html">Thoughts</a>
+			<a href="/modding_guide/modding_guide/intermediate/traits/index.html">Traits</a>
+			<a href="/modding_guide/modding_guide/intermediate/biomes/index.html">Biomes</a>
 			<div>
-				<a href="/modding_guide/intermediate/biomes/seasons_weather/index.html">Seasons and weather</a>
+				<a href="/modding_guide/modding_guide/intermediate/biomes/seasons_weather/index.html">Seasons and weather</a>
 			</div>
-			<a href="/modding_guide/intermediate/jobs/index.html">Adding jobs</a>
+			<a href="/modding_guide/modding_guide/intermediate/jobs/index.html">Adding jobs</a>
 			<div>
-				<a href="/modding_guide/intermediate/jobs/crafting_jobs/index.html">Crafting jobs</a>
-				<a href="/modding_guide/intermediate/jobs/combat_jobs/index.html">Combat jobs</a>
+				<a href="/modding_guide/modding_guide/intermediate/jobs/crafting_jobs/index.html">Crafting jobs</a>
+				<a href="/modding_guide/modding_guide/intermediate/jobs/combat_jobs/index.html">Combat jobs</a>
 			</div>
-			<a href="/modding_guide/intermediate/kingdoms/index.html">Kingdoms</a>
-			<a href="/modding_guide/intermediate/monsters/index.html">Critters and monsters</a>
-			<a href="/modding_guide/intermediate/campaigns/index.html">Adding campaigns</a>
+			<a href="/modding_guide/modding_guide/intermediate/kingdoms/index.html">Kingdoms</a>
+			<a href="/modding_guide/modding_guide/intermediate/monsters/index.html">Critters and monsters</a>
+			<a href="/modding_guide/modding_guide/intermediate/campaigns/index.html">Adding campaigns</a>
 			<div>
-				<a href="/modding_guide/intermediate/campaigns/encounters/index.html">Generic encounters</a>
+				<a href="/modding_guide/modding_guide/intermediate/campaigns/encounters/index.html">Generic encounters</a>
 			</div>
-			<a href="/modding_guide/intermediate/hotloading/index.html">Hotloading mods</a>
-			<a href="/modding_guide/intermediate/testing/index.html">Testing your mod</a>
+			<a href="/modding_guide/modding_guide/intermediate/hotloading/index.html">Hotloading mods</a>
+			<a href="/modding_guide/modding_guide/intermediate/testing/index.html">Testing your mod</a>
 		</div>
-		<a href="/modding_guide/advanced/index.html">Advanced Modding</a>
+		<a href="/modding_guide/modding_guide/advanced/index.html">Advanced Modding</a>
 		<div>
-			<a href="/modding_guide/advanced/logging/index.html">User settings and logging</a>
-			<a href="/modding_guide/advanced/events/index.html">Events</a>
-			<a href="/modding_guide/advanced/components_controllers/index.html">Lua scripts</a>
+			<a href="/modding_guide/modding_guide/advanced/logging/index.html">User settings and logging</a>
+			<a href="/modding_guide/modding_guide/advanced/events/index.html">Events</a>
+			<a href="/modding_guide/modding_guide/advanced/components_controllers/index.html">Lua scripts</a>
 			<div>
-				<a href="/modding_guide/advanced/components_controllers/components_renderers/index.html">Components</a>
-				<a href="/modding_guide/advanced/components_controllers/controllers/index.html">Controllers</a>
+				<a href="/modding_guide/modding_guide/advanced/components_controllers/components_renderers/index.html">Components</a>
+				<a href="/modding_guide/modding_guide/advanced/components_controllers/controllers/index.html">Controllers</a>
 			</div>
-			<a href="/modding_guide/advanced/ai/index.html">AI actions</a>
-			<a href="/modding_guide/advanced/services/index.html">Services</a>
-			<a href="/modding_guide/advanced/call_handlers/index.html">Call handlers</a>
-			<a href="/modding_guide/advanced/ui/index.html">UI</a>
-			<a href="/modding_guide/advanced/cpp/index.html">C++ functions</a>
+			<a href="/modding_guide/modding_guide/advanced/ai/index.html">AI actions</a>
+			<a href="/modding_guide/modding_guide/advanced/services/index.html">Services</a>
+			<a href="/modding_guide/modding_guide/advanced/call_handlers/index.html">Call handlers</a>
+			<a href="/modding_guide/modding_guide/advanced/ui/index.html">UI</a>
+			<a href="/modding_guide/modding_guide/advanced/cpp/index.html">C++ functions</a>
 		</div>
 	</section>
 </nav>
