6dcd5a9d42d50dfa5c6d0794c1fc6f28b2c2c913
Bruno Mussoi Mendonça <bruno_mussoi@hotmail.com>
2024-08-29 16:39:06 -0300

    accessibility

--- menus.js
+++ menus.js
@@ -1,6 +1,6 @@
 document.currentScript.outerHTML=`
 <header>
-	<img src="/modding_guide/images/stonehearth-logo.png"/>
+	<a href="/modding_guide/index.html"><img alt="Stonehearth Logo" src="/modding_guide/images/stonehearth-logo.png"/></a>
 </header>
 <a id="go_to_the_top_button" href="#top">TOP</a>
 <nav>
